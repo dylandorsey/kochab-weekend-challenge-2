23848eb43d9ae6f3f19971432489af5b75b199dc
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-21 14:43:34 -0500

    history display on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,6 +8,8 @@ let newOperation = {
     operation: ''
 }
 
+let operationHistoryArray = [];
+
 function onReady() {
     console.log('in jQ');
     $('#addBtn').on('click', addNumbers);
@@ -22,24 +24,29 @@ function addNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'add';
     postNewOperation(newOperation);
+    getOperationHistory();
+
 }
 
 function subtractNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'subtract';
     postNewOperation(newOperation);
+    getOperationHistory();
 }
 
 function divideNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'divide';
     postNewOperation(newOperation);
+    getOperationHistory();
 }
 
 function multiplyNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'multiply';
     postNewOperation(newOperation);
+    getOperationHistory();
 }
 
 function deleteHistory() {
@@ -53,14 +60,12 @@ function insertNumbers() {
 }
 
 function postNewOperation(newOperation) {
-    console.log(newOperation);
     $.ajax({
         method: 'POST',
         url: '/new-operation',
         data: newOperation
     })
         .then(function (response) {
-            console.log(response);
         });
 }
 
@@ -69,7 +74,39 @@ function getOperationHistory() {
         type: 'GET',
         url: '/operation-history'
     })
-    .then(function (response) {
-        console.log(response);
-    });
+        .then(function (response) {
+            console.log(response);
+            displayOperationHistory(response);
+        });
+}
+
+function displayOperationHistory(historyArray) {
+    // for (operationObject in historyArray) {
+    //     if (operationObject.operation = 'add') {
+    //         operation = '+';
+    //     }
+    //     else if (operationObject.operation = 'subtract') {
+    //         operation = '-';
+    //     }
+    //     else if (operationObject.operation = 'multiply') {
+    //         operation = '*';
+    //     }
+    //     else if (operationObject.operation = 'divide') {
+    //         operation = '/';
+    //     }
+    // }
+    emptyHistoryDiv();
+    for (let i = 0; i<historyArray.length; i++) {
+        let numberOne = historyArray[i].numberOne;
+        let numberTwo = historyArray[i].numberTwo;
+        let operation = historyArray[i].operation;
+        let result = historyArray[i].result
+        $('#historyDiv').prepend(`<p>
+        ${numberOne} ${operation} ${numberTwo} = ${result}
+    </p>`);
+    }
+}
+
+function emptyHistoryDiv() {
+    $('#historyDiv').empty();
 }
