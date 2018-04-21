f6d152a13bce341b23e172b4e713028cc20104cd
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-21 07:11:57 -0500

    server performs operation

--- server/server.js
+++ server/server.js
@@ -16,6 +16,45 @@ const operationHistoryArray = []
 app.post('/new-operation', (req, res) => {
     console.log(req.body); 
     operationHistoryArray.push(req.body);
-    console.log(operationHistoryArray);
+    req.body.numberOne = Number(req.body.numberOne);
+    req.body.numberTwo = Number (req.body.numberTwo);
+    operation = req.body.operation;
     res.sendStatus(200);
+    if (operation == 'add') {
+        result =  add(req.body);
+    }
+    else if (operation == 'subtract') {
+        result = subtract(req.body);
+    }
+    else if (operation == 'multiply') {
+        result = multiply(req.body);
+    }
+    else if (operation == 'divide') {
+        result = divide(req.body);
+    }
 });// end app.post
+
+function add (arrayObject) {
+    result = arrayObject.numberOne + arrayObject.numberTwo;
+    console.log(result);
+    return result;
+}
+
+function subtract (arrayObject) {
+    result = arrayObject.numberOne - arrayObject.numberTwo;
+    console.log(result);
+    return result;
+}
+
+
+function multiply (arrayObject) {
+    result = arrayObject.numberOne * arrayObject.numberTwo;
+    console.log(result);
+    return result;
+}
+
+function divide (arrayObject) {
+    result = arrayObject.numberOne / arrayObject.numberTwo;
+    console.log(result);
+    return result;
+}
