b6fb907e41da7de94c4a05d86c62a65c4efe6189
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-21 16:21:22 -0500

    base mode functionality with comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,14 +2,27 @@ console.log('in js');
 
 $(document).ready(onReady);
 
+/* Client-side script captures user inputs, posts them to the server for server-side manipulation,
+and requests results from the server.
+User input is captured by assigning it to keys of an object.
+The inputs include two value inputs and math operation type (addition, subtraction, multiplication, division).
+After posting to the server, the client script requests the history of math operations from the server,
+and appends the history to the DOM.
+The user may click a button to delete the history. Upon clicking, the client script makes a server request.
+Upon this request, the server clears the history data stored there and returns the (now empty) history data array.
+This empty array is displayed on the DOM, effectively clearing the history both server- and client-side.*/
+
+// this object will be posted to the server after its keys have been assigned values by further functions
 let newOperation = {
     numberOne: '',
     numberTwo: '',
     operation: ''
 }
 
+// this client-side variable is later used to capture history data from the server.
 let operationHistoryArray = [];
 
+// add event listeners on buttons and display history information to the DOM
 function onReady() {
     console.log('in jQ');
     $('#addBtn').on('click', addNumbers);
@@ -18,47 +31,49 @@ function onReady() {
     $('#divideBtn').on('click', divideNumbers);
     $('#deleteHistoryBtn').on('click', clearOperationHistory);
     getOperationHistory();
-}
+}// end onReady
 
+
+// addition functionality
 function addNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'add';
     postNewOperation(newOperation);
     getOperationHistory();
+}// end addNumbers
 
-}
-
+// subtraction functionality
 function subtractNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'subtract';
     postNewOperation(newOperation);
     getOperationHistory();
-}
+}// end subtractNumbers
 
+// division functionality
 function divideNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'divide';
     postNewOperation(newOperation);
     getOperationHistory();
-}
+}// end divideNumbers
 
+// multiplication functionality
 function multiplyNumbers() {
     newOperation = insertNumbers();
     newOperation.operation = 'multiply';
     postNewOperation(newOperation);
     getOperationHistory();
-}
+}// end multiplyNumbers
 
-function deleteHistory() {
-    console.log('clicked delete history');
-}
-
+// assign input values to operation object
 function insertNumbers() {
     newOperation.numberOne = $('#numberOne').val();
     newOperation.numberTwo = $('#numberTwo').val();
     return newOperation;
-}
+}// end insertNumbers
 
+// send data (input field values and operation type) to server
 function postNewOperation(newOperation) {
     $.ajax({
         method: 'POST',
@@ -67,8 +82,9 @@ function postNewOperation(newOperation) {
     })
         .then(function (response) {
         });
-}
+}// end postNewOperation
 
+// request operation history array from server
 function getOperationHistory() {
     $.ajax({
         type: 'GET',
@@ -77,8 +93,9 @@ function getOperationHistory() {
         .then(function (response) {
             displayOperationHistory(response);
         });
-}
+}// end getOperationHistory
 
+// request to empty history array on server
 function clearOperationHistory() {
     $.ajax({
         type: 'GET',
@@ -87,10 +104,13 @@ function clearOperationHistory() {
         .then(function (response) {
             displayOperationHistory(response);
         });
-}
+}// end clearOperationHistory
 
+// loop through history array and prepend data to DOM
 function displayOperationHistory(historyArray) {
+    // clear the history div on DOM
     emptyHistoryDiv();
+    // loop and prepend data to DOM
     for (let i = 0; i < historyArray.length; i++) {
         let numberOne = historyArray[i].numberOne;
         let numberTwo = historyArray[i].numberTwo;
@@ -100,8 +120,8 @@ function displayOperationHistory(historyArray) {
         ${numberOne} ${operation} ${numberTwo} = ${result}
     </p>`);
     }
-}
+}// end displayOperationHistory
 
 function emptyHistoryDiv() {
     $('#historyDiv').empty();
-}
+}// end emptyHistoryDiv
--- server/server.js
+++ server/server.js
@@ -3,26 +3,38 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+// serve static files
 app.use(express.static('server/public'));
+
+// enable body parser functionality
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// listen for requests on PORT
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
 
+// array used for storing history of math operations sent from client
 const operationHistoryArray = []
 
+// responds to client with history of math operations
 app.get('/operation-history', (req, res) => {
     res.send(operationHistoryArray);
 });
 
+// receives client data, performs math operation, and stores input-and-result data server-side
 app.post('/new-operation', (req, res) => {
-    console.log(req.body);
+    // store number inputs
     req.body.numberOne = Number(req.body.numberOne);
     req.body.numberTwo = Number(req.body.numberTwo);
+    // store operation input
     operation = req.body.operation;
+    // respond that POST was successfull
     res.sendStatus(200);
+    // determine which math operation to perform,
+    // convert text to math symbol,
+    // call math operation function,
+    // store result as a variable
     if (operation == 'add') {
         req.body.operation = '+';
         result = add(req.body);
@@ -39,37 +51,39 @@ app.post('/new-operation', (req, res) => {
         req.body.operation = '/';
         result = divide(req.body);
     }
+    // assign math operation result to math data object key
     req.body.result = result;
     operationHistoryArray.push(req.body);
     console.log(operationHistoryArray);
 });// end app.post
 
+// upon request, clear data from operation history array
 app.get('/clear-history', (req, res) => {
     operationHistoryArray.splice(0, operationHistoryArray.length);
     res.send(operationHistoryArray);
 });// end app.post
 
+// addition operation
 function add(arrayObject) {
     result = arrayObject.numberOne + arrayObject.numberTwo;
-    console.log(result);
     return result;
-}
+}// end addition operation
 
+// subtraction operation
 function subtract(arrayObject) {
     result = arrayObject.numberOne - arrayObject.numberTwo;
-    console.log(result);
     return result;
-}
+}// end subtraction operation
 
-
+// multiplication operation
 function multiply(arrayObject) {
     result = arrayObject.numberOne * arrayObject.numberTwo;
-    console.log(result);
     return result;
-}
+}// end multiplication operation
 
+
+// division operation
 function divide(arrayObject) {
     result = arrayObject.numberOne / arrayObject.numberTwo;
-    console.log(result);
     return result;
-}
+}// end division operation
